d1c59b555181715d9853a0f21662466392dde47e
seawboon <seawboon.yeap@gmail.com>
2020-07-19 16:05:22 +0800

    udpate

--- resources/views/patient/edit.blade.php
+++ resources/views/patient/edit.blade.php
@@ -374,6 +374,21 @@
 </div>
 </div>
 </div>
+
+<script>
+$(function(){
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var recipient = button.data('whatever'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    modal.find('.modal-title').text(recipient);
+    modal.find('.modalimage').attr('src', recipient);
+  });
+});
+</script>
+
 <script>
 $(document).ready(function() {
   $(function () {
